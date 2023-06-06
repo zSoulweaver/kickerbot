eaaf3849abee78fe82152f9ae7271a37dcc93abf
Matt <aspifie@gmail.com>
2023-06-06 23:23:51 +1000

    feat: support quotes strings in arguments

--- apps/nest-bot/src/kicker/commands/arguments-pipe.ts
+++ apps/nest-bot/src/kicker/commands/arguments-pipe.ts
@@ -11,18 +11,41 @@ export class ArgumentsPipe implements PipeTransform<any> {
 
     const argumentObject = {}
 
-    for (const [i, property] of propertyNames.entries()) {
-      const typeInfo = getProperty(type as TypeClass, property)
-      if (i === propertyNames.length - 1) {
-        // Last Loop - Check if we have an array type to fill
-        if (typeInfo?.type.kind === ReflectionKind.array) {
-          argumentObject[property] = value.slice(i)
-        } else {
-          argumentObject[property] = value[i]
-        }
-      } else {
-        argumentObject[property] = value[i]
+    const newValue: string[] = []
+    const skippedIndexes: number[] = []
+    for (let i = 0; i < value.length; i++) {
+      if (skippedIndexes.includes(i)) {
+        continue
       }
+
+      const arg = value[i]
+
+      if (arg.startsWith('"')) {
+        const searchArray = value.slice(i)
+        const searchEnd = searchArray.findIndex(x => x.endsWith('"'))
+        if (searchEnd >= 0) {
+          const quoteValue = searchArray.slice(0, searchEnd + 1)
+          const finalValue = quoteValue.join(' ').replace(/['"]+/g, '')
+          const indexesToSkip = Array.from({ length: quoteValue.length }, (_, x) => x + i)
+          skippedIndexes.push(...indexesToSkip)
+          newValue.push(finalValue)
+          continue
+        }
+      }
+
+      newValue.push(arg)
+    }
+
+    for (const [i, property] of propertyNames.entries()) {
+      if (i === propertyNames.length - 1) { // Last Loop - Check if we have an array type to fill
+        const typeInfo = getProperty(type as TypeClass, property)
+        if (typeInfo?.type.kind === ReflectionKind.array) {
+          argumentObject[property] = newValue.slice(i)
+          continue
+        }
+      }
+
+      argumentObject[property] = newValue[i]
     }
 
     const deserialized = deserialize<typeof DTOType>(argumentObject as any)
